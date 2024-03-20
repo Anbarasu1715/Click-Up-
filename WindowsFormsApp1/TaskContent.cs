a0b95654ce1502dfbd9d4944a288e90009b1e48a
ANBARASU M <anbarasu47442003@gmail.com>
2024-03-20 15:14:44 +0530

    Taskviewer

---
+++ WindowsFormsApp1/TaskContent.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public class TaskContent
+    {
+        public string ProjectName { get; set; }
+        private string Duration;
+
+        public string TaskName { get; set; }
+        public string Assignor { get; set; }
+        public System.Windows.Forms.Label Status { get; set; }
+        public string DueDate { get; set; }
+        public System.Windows.Forms.Label Priority { get; set; }
+        public string LastModified { get; set; }
+        public Button View { get; set; }
+
+
+        public TaskContent(string projectName, string taskName, string assignor, string dueDate, string lastModified,System.Windows.Forms.Label statusLabel, System.Windows.Forms.Label priorityLabel, Button ViewBtn)
+        {
+            ProjectName = projectName;
+            TaskName = taskName;
+            Assignor = assignor;
+            DueDate = dueDate;
+            LastModified = lastModified;
+            Status = statusLabel;
+            Priority = priorityLabel;
+            View = ViewBtn;
+        }
+
+        public string GetDuration()
+        {
+            return Duration;
+        }
+        public void SetDuration(string start, string end)
+        {
+            Duration = start + " - " + end;
+        }
+    }
+}
---
+++ WindowsFormsApp1/UCTaskViewer.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class UCTaskViewer
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TitlePanel = new System.Windows.Forms.TableLayoutPanel();
+            this.OuterPanel = new System.Windows.Forms.Panel();
+            this.HeaderPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.ColorPanel = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.ContentPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.OuterPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TitlePanel
+            // 
+            this.TitlePanel.ColumnCount = 1;
+            this.TitlePanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.TitlePanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TitlePanel.Location = new System.Drawing.Point(0, 0);
+            this.TitlePanel.Name = "TitlePanel";
+            this.TitlePanel.RowCount = 2;
+            this.TitlePanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.TitlePanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.TitlePanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.TitlePanel.Size = new System.Drawing.Size(1150, 70);
+            this.TitlePanel.TabIndex = 0;
+            // 
+            // OuterPanel
+            // 
+            this.OuterPanel.Controls.Add(this.ContentPanel);
+            this.OuterPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.OuterPanel.Location = new System.Drawing.Point(7, 115);
+            this.OuterPanel.Name = "OuterPanel";
+            this.OuterPanel.Size = new System.Drawing.Size(1143, 51);
+            this.OuterPanel.TabIndex = 1;
+            // 
+            // HeaderPanel
+            // 
+            this.HeaderPanel.ColumnCount = 7;
+            this.HeaderPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.HeaderPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.HeaderPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.HeaderPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.HeaderPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.HeaderPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.HeaderPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.HeaderPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.HeaderPanel.Location = new System.Drawing.Point(7, 70);
+            this.HeaderPanel.Name = "HeaderPanel";
+            this.HeaderPanel.RowCount = 1;
+            this.HeaderPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.HeaderPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 45F));
+            this.HeaderPanel.Size = new System.Drawing.Size(1143, 45);
+            this.HeaderPanel.TabIndex = 3;
+            // 
+            // ColorPanel
+            // 
+            this.ColorPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.ColorPanel.Location = new System.Drawing.Point(0, 70);
+            this.ColorPanel.Name = "ColorPanel";
+            this.ColorPanel.Size = new System.Drawing.Size(7, 96);
+            this.ColorPanel.TabIndex = 5;
+            // 
+            // panel3
+            // 
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(0, 166);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1150, 50);
+            this.panel3.TabIndex = 6;
+            // 
+            // ContentPanel
+            // 
+            this.ContentPanel.ColumnCount = 7;
+            this.ContentPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.ContentPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.ContentPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.ContentPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.ContentPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.ContentPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.ContentPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.ContentPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ContentPanel.Location = new System.Drawing.Point(0, 0);
+            this.ContentPanel.Name = "ContentPanel";
+            this.ContentPanel.RowCount = 1;
+            this.ContentPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.ContentPanel.Size = new System.Drawing.Size(1143, 50);
+            this.ContentPanel.TabIndex = 0;
+            // 
+            // UCTaskViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.OuterPanel);
+            this.Controls.Add(this.HeaderPanel);
+            this.Controls.Add(this.ColorPanel);
+            this.Controls.Add(this.TitlePanel);
+            this.Controls.Add(this.panel3);
+            this.Name = "UCTaskViewer";
+            this.Size = new System.Drawing.Size(1150, 216);
+            this.OuterPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel TitlePanel;
+        private System.Windows.Forms.Panel OuterPanel;
+        private System.Windows.Forms.TableLayoutPanel HeaderPanel;
+        private System.Windows.Forms.Panel ColorPanel;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TableLayoutPanel ContentPanel;
+    }
+}
